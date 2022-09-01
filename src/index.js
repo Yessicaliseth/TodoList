aa99c538de910926724fd9ab590f64d06cd741da
Yessica <yessicalistrivino@gmail.com>
2022-09-01 11:50:39 -0500

    Modificacion del index.js React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './components/App/index.js';
 import './index.css';
 
-
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = ReactDOMClient.createRoot(rootElement)
+root.render(
      <App />,
-     document.getElementById('root')
+     
 );
 
 
